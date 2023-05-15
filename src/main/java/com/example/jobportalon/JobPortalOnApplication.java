8ddc6ec48fe9f6c4fb3aaf8e95daba932e681ac9
franceska harizaj <franceskaharizaj5@gmail.com>
2023-05-15 12:36:22 +0100

    first commit

---
+++ src/main/java/com/example/jobportalon/JobPortalOnApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobportalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalOnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jobportalon/JobPortalOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobportalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
